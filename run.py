52fe49b2ec019dd98b33d2b3d2733aa174b14c86
theholacking <bashir.developer@gmail.com>
2025-10-01 11:37:16 +0300

    update DB cert path for Render

--- run.py
+++ run.py
@@ -24,7 +24,7 @@ app.config['MYSQL_HOST'] = os.getenv("MYSQL_HOST")
 app.config['MYSQL_USER'] = os.getenv("MYSQL_USER")
 app.config['MYSQL_PASSWORD'] = os.getenv("MYSQL_PASSWORD")
 app.config['MYSQL_PORT'] = int(os.getenv("MYSQL_PORT"))
-app.config['MYSQL_SSL_CA'] = os.getenv("MYSQL_SSL_CA")
+app.config['MYSQL_SSL_CA'] =os.path.join(os.getcwd(), "certs/ca.pem")
 app.config['MYSQL_DB'] = os.getenv("MYSQL_DB")
 mysql=MySQL(app)
 
